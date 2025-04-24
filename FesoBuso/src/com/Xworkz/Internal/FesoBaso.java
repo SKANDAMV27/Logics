9a0b772ee979cddf3db32a8100bf8ea451225a56
Skanda <skandagowda0@gmail.com>
2025-04-24 07:33:06 +0530

    if condition Logics

---
+++ FesoBuso/src/com/Xworkz/Internal/FesoBaso.java
@@ -0,0 +1,22 @@
+package com.Xworkz.Internal;
+
+public class FesoBaso {
+    public int num=5;
+
+    {
+        for (int i = 1; i <= 1; i++) {
+            if(num%3==0 && num%5==0){
+                System.out.println(+num+": FesoBaso");
+            } else if (num%5==0) {
+                System.out.println(+num+" :Baso");
+            } else if (num%3==0) {
+                System.out.println(+num+": This is Feso");
+
+            } else {
+                System.out.println("Not are not");
+            }
+
+
+        }
+    }
+}
---
+++ FesoBuso/src/com/Xworkz/MainRunner/Runner.java
@@ -0,0 +1,10 @@
+package com.Xworkz.MainRunner;
+
+import com.Xworkz.Internal.FesoBaso;
+
+public class Runner {
+    public static void main(String[] args) {
+        FesoBaso fesoBaso = new FesoBaso();
+
+    }
+}
